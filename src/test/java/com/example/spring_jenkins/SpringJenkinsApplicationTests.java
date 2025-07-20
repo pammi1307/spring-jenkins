030b0e4a547b7984a82f5cff06e5f7d567ee9e5f
Pramoda S <pramodas1307@gmail.com>
2025-07-20 11:50:10 +0530

    nor error guessing

--- src/test/java/com/example/spring_jenkins/SpringJenkinsApplicationTests.java
+++ src/test/java/com/example/spring_jenkins/SpringJenkinsApplicationTests.java
@@ -14,7 +14,7 @@ class SpringJenkinsApplicationTests {
 	public static Logger logger = LoggerFactory.getLogger(SpringJenkinsApplicationTests.class);
 
 	@Test
-	public  contextLoads() {
+	public void contextLoads() {
 		logger.info("Test case executig..............");
 		logger.info("Test case executig second log statement..............");
 		assertEquals(true,true);
