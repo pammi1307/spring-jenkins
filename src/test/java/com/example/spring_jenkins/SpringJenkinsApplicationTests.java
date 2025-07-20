96f691a427086f807d632fa86c8e51a1e712eda1
Pramoda S <pramodas1307@gmail.com>
2025-07-20 11:40:23 +0530

    forcefully build failure

--- src/test/java/com/example/spring_jenkins/SpringJenkinsApplicationTests.java
+++ src/test/java/com/example/spring_jenkins/SpringJenkinsApplicationTests.java
@@ -14,7 +14,7 @@ class SpringJenkinsApplicationTests {
 	public static Logger logger = LoggerFactory.getLogger(SpringJenkinsApplicationTests.class);
 
 	@Test
-	void contextLoads() {
+	public  contextLoads() {
 		logger.info("Test case executig..............");
 		logger.info("Test case executig second log statement..............");
 		assertEquals(true,true);
